49154af875f324edfc3521bff801c0974974ce8f
rikimario <mario_martinov@abv.bg>
2024-03-28 23:39:33 +0200

    add register and login forums

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,10 @@
-export default function Login({ closeLogin }) {
+export default function Login({ closeLogin, handleCloseLogin }) {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm">
+    <div
+      onClick={handleCloseLogin}
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm"
+      id="login"
+    >
       <div className="flex w-[600px] flex-col">
         <button
           onClick={closeLogin}
@@ -8,7 +12,72 @@ export default function Login({ closeLogin }) {
         >
           X
         </button>
-        <div className="rounded bg-white p-2 text-black">Login</div>
+        <form
+          className="space-y-6 rounded-lg bg-[#28262D] p-8"
+          action="#"
+          method="POST"
+        >
+          <div className="flex items-center justify-center text-3xl font-bold text-white">
+            <h1>CinemaHub</h1>
+          </div>
+
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-300"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-300"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-[#00925D] hover:text-[#00925de8]"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-[#00925D] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#00925de8] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#00925D]"
+            >
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,6 +1,10 @@
-export default function Register({ closeRegister }) {
+export default function Register({ closeRegister, handleCloseRegister }) {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm">
+    <div
+      onClick={handleCloseRegister}
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm"
+      id="register"
+    >
       <div className="flex w-[600px] flex-col">
         <button
           onClick={closeRegister}
@@ -8,7 +12,84 @@ export default function Register({ closeRegister }) {
         >
           X
         </button>
-        <div className="rounded bg-white p-2 text-black">Register</div>
+        <form
+          className="space-y-6 rounded-lg bg-[#28262D] p-8"
+          action="#"
+          method="POST"
+        >
+          <div className="flex items-center justify-center text-3xl font-bold text-white">
+            <h1>CinemaHub</h1>
+          </div>
+
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium leading-6 text-gray-300"
+            >
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                id="username"
+                name="username"
+                type="username"
+                autoComplete="username"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-300"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-300"
+              >
+                Password
+              </label>
+              <div className="text-sm"></div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-[#00925D] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#00925de8] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#00925D]"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
