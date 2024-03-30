57cff74fec6023ed5704113669d8d3a82d4305e6
rikimario <mario_martinov@abv.bg>
2024-03-30 23:34:55 +0200

    add font and padding to register inputs

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -46,7 +46,7 @@ export default function Login({ closeLogin, handleCloseLogin }) {
                 type="email"
                 autoComplete="email"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 pl-2 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -62,7 +62,7 @@ export default function Login({ closeLogin, handleCloseLogin }) {
               <div className="text-sm">
                 <a
                   href="#"
-                  className="font-semibold text-[#00925D] hover:text-[#00925de8]"
+                  className="pl-2 font-semibold font-semibold text-[#00925D] hover:text-[#00925de8]"
                 >
                   Forgot password?
                 </a>
@@ -75,7 +75,7 @@ export default function Login({ closeLogin, handleCloseLogin }) {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 pl-2 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00925D] sm:text-sm sm:leading-6"
               />
             </div>
           </div>
