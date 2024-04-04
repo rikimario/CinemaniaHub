fcb0d766b5a95ca6b2fa480410f8c01df23d4436
rikimario <mario_martinov@abv.bg>
2024-04-04 23:44:26 +0300

    add slider to upcoming

--- src/components/upcoming/Upcoming.jsx
+++ src/components/upcoming/Upcoming.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const apiUrl =
   "https://api.themoviedb.org/3/movie/upcoming?api_key=589f3d4f48689702b074a222aea6db87";
 
 export default function Upcoming() {
   const [movies, setMovies] = useState([]);
-  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     try {
       fetch(apiUrl)
         .then((res) => res.json())
         .then((data) => {
-          const firstFiveMovies = data.results.slice(0, 5);
+          const firstFiveMovies = data.results.slice(0, 10);
           setMovies(firstFiveMovies);
         });
     } catch (error) {
@@ -20,36 +22,32 @@ export default function Upcoming() {
     }
   }, []);
 
-  const prevSlide = () => {
-    setCurrentSlide((prevSlide) =>
-      prevSlide === 0 ? movies.length - 1 : prevSlide - 1,
-    );
-  };
-
-  const nextSlide = () => {
-    setCurrentSlide((prevSlide) =>
-      prevSlide === movies.length - 1 ? 0 : prevSlide + 1,
-    );
+  const settings = {
+    slidesToShow: 5, // Display 5 images in a row
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    prevArrow: <button>Previous</button>,
+    nextArrow: <button>Next</button>,
   };
   return (
-    <div className="relative h-full w-full overflow-hidden px-11 pt-12">
+    <div className="px-11 pt-12">
       <h1 className="pb-4 text-2xl text-white">Upcoming</h1>
-
-      <div className="grid w-full grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-5">
-        {movies.map((movie, index) => (
-          <div
-            className={`relative flex h-[30rem] w-full items-center justify-center ${
-              index === 0 ? "" : "hidden md:flex lg:block"
-            }`}
-            key={index}
-          >
-            <img
-              className="absolute h-full w-auto rounded-xl   bg-black object-fill opacity-50"
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.title}
-            />
-          </div>
-        ))}
+      <div className="">
+        <Slider {...settings}>
+          {movies.map((movie, index) => (
+            <div
+              className="h-[15rem] w-full md:h-[18rem] lg:h-[27rem]"
+              key={index}
+            >
+              <img
+                className="h-full w-auto rounded-xl bg-black object-fill opacity-50"
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
