bbd3ffe3a99a0e89e0e116dfbc2ac359bb382f82
rikimario <mario_martinov@abv.bg>
2024-04-10 18:24:34 +0300

    add login and register logic, middleware and database

--- server/index.js
+++ server/index.js
@@ -1,46 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-const bcrypt = require("bcrypt");
-const User = require("./models/User");
+const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
 const PORT = 5000;
 
 const app = express();
+
+//* middleware //
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-mongoose.connect("mongodb://127.0.0.1:27017/cinema-hub");
+app.use("/", require("./routes/authRoutes"));
 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  User.findOne({ email: email }).then((user) => {
-    if (user) {
-      bcrypt.compare(password, user.password, (err, result) => {
-        if (result) {
-          res.json("Success");
-        } else {
-          res.json("Email or Password is incorrect");
-        }
-      });
-    } else {
-      res.json('User don"t exist');
-    }
-  });
-});
-
-app.post("/register", (req, res) => {
-  const { username, email, password } = req.body;
-  bcrypt.hash(password, 10, (err, hash) => {
-    if (err) {
-      res.json(err);
-    } else {
-      User.create({ username: username, email: email, password: hash })
-        .then((user) => res.json(user))
-        .catch((err) => res.json(err));
-    }
-  });
-});
+// * database connection //
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cinema-hub")
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("Database not Connected", err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
