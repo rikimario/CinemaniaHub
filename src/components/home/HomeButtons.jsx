e981e6734c96feef67c8574423b1e7860186bd70
rikimario <mario_martinov@abv.bg>
2024-04-12 22:35:03 +0300

    add path to movie details

--- src/components/home/HomeButtons.jsx
+++ src/components/home/HomeButtons.jsx
@@ -1,12 +1,18 @@
-export default function HomeButtons() {
+import Path from "@/paths/paths";
+import { Link } from "react-router-dom";
+
+export default function HomeButtons({ id }) {
   return (
     <div className="relative z-40 space-x-4 pt-8 lg:flex">
-      <button className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md border px-3 text-lg font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50">
+      <Link
+        to={`${Path.MovieDetails}/${id}`}
+        className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md border px-3 text-lg font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
+      >
         Read More
-      </button>
-      <button className="focus-visible:ring-ring text-destructive-foreground hover:bg-destructive/90 inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md bg-[#00925D] px-3 text-lg font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50">
+      </Link>
+      <Link className="focus-visible:ring-ring text-destructive-foreground hover:bg-destructive/90 inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md bg-[#00925D] px-3 text-lg font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50">
         Add to Watchlist
-      </button>
+      </Link>
     </div>
   );
 }
