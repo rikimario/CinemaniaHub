532d751950698267c845fa6a7bce2730efdb0b17
rikimario <mario_martinov@abv.bg>
2024-04-13 23:34:48 +0300

    add storage context to favorite page

--- src/components/favoriteMovies/FavoriteMovies.jsx
+++ src/components/favoriteMovies/FavoriteMovies.jsx
@@ -1,35 +1,23 @@
+import { useContext } from "react";
 import { AspectRatio } from "../ui/aspect-ratio";
+import { StorageContext } from "@/context/storageContext";
 
 export default function FavoriteMovies() {
+  const { favorite } = useContext(StorageContext);
   return (
     <>
       <div className="pl-11 pt-6">
-        <h1 className="pb-4 text-2xl">Favorite movies</h1>
-
-        <div className="grid grid-cols-3 gap-8 py-6">
-          <div className="w-[450px] bg-white">
-            <AspectRatio ratio={16 / 9}>
-              <img src="" alt="" />
-            </AspectRatio>
-          </div>
-
-          <div className="w-[450px] bg-white">
-            <AspectRatio ratio={16 / 9}>
-              <img src="" alt="" />
-            </AspectRatio>
-          </div>
-
-          <div className="w-[450px] bg-white">
-            <AspectRatio ratio={16 / 9}>
-              <img src="" alt="" />
-            </AspectRatio>
-          </div>
-
-          <div className="w-[450px] bg-white">
-            <AspectRatio ratio={16 / 9}>
-              <img src="" alt="" />
-            </AspectRatio>
-          </div>
+        <h1 className="text-2xl">Favorite movies</h1>
+        <div className="grid grid-cols-5 py-10">
+          {favorite.map((movie) => (
+            <div className="w-56">
+              <img
+                className="h-full w-auto rounded-xl object-fill opacity-50"
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
