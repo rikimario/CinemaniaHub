7a1583fc3da947a6152ac548883fb34dfcf04f7c
rikimario <mario_martinov@abv.bg>
2024-04-14 22:42:44 +0300

    add remove and add state and action to app reducer

--- src/context/appReducer.jsx
+++ src/context/appReducer.jsx
@@ -1,9 +1,47 @@
 export default (state, action) => {
   switch (action.type) {
+    // FAVORITE MOVIE //
     case "ADD_MOVIE_TO_FAVORITE":
       return {
         ...state,
+        watched: state.watched.filter(
+          (movie) => movie.id !== action.payload.id,
+        ),
         favorite: [action.payload, ...state.favorite],
+      };
+    case "REMOVE_MOVIE_FROM_FAVORITE":
+      return {
+        ...state,
+        favorite: state.favorite.filter((movie) => movie.id !== action.payload),
+      };
+
+    // WATCHED MOVIE //
+    case "ADD_MOVIE_TO_WATCHED":
+      return {
+        ...state,
+        watchlist: state.watchlist.filter(
+          (movie) => movie.id !== action.payload.id,
+        ),
+        watched: [action.payload, ...state.watched],
+      };
+    case "REMOVE_FROM_WATCHED":
+      return {
+        ...state,
+        watched: state.watched.filter((movie) => movie.id !== action.payload),
+      };
+
+    // WATCHLIST //
+    case "ADD_MOVIE_TO_WATCHLIST":
+      return {
+        ...state,
+        watchlist: [action.payload, ...state.watchlist],
+      };
+    case "REMOVE_MOVIE_FROM_WATCHLIST":
+      return {
+        ...state,
+        watchlist: state.watchlist.filter(
+          (movie) => movie.id !== action.payload,
+        ),
       };
     default:
       return state;
