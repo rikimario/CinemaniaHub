7555c1119646ed5cd84dd50ee081c7ecc0a7271f
rikimario <mario_martinov@abv.bg>
2024-04-14 22:45:31 +0300

    add movie controls

--- src/components/favoriteMovies/FavoriteMovies.jsx
+++ src/components/favoriteMovies/FavoriteMovies.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
-import { AspectRatio } from "../ui/aspect-ratio";
 import { StorageContext } from "@/context/storageContext";
+import MovieControlls from "../movie-controlls/MovieControlls";
 
-export default function FavoriteMovies() {
+export default function FavoriteMovies({ movie, type }) {
   const { favorite } = useContext(StorageContext);
   return (
     <>
@@ -10,12 +10,13 @@ export default function FavoriteMovies() {
         <h1 className="text-2xl">Favorite movies</h1>
         <div className="grid grid-cols-5 py-10">
           {favorite.map((movie) => (
-            <div className="w-56">
+            <div key={movie.id} className="w-56 pb-6">
               <img
                 className="h-full w-auto rounded-xl object-fill opacity-50"
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                 alt={movie.title}
               />
+              <MovieControlls type={type} movie={movie} />
             </div>
           ))}
         </div>
