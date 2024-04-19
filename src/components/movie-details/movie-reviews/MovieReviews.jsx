23a76c7bd8ddb70927374bc1d41c659e9c265d2d
rikimario <mario_martinov@abv.bg>
2024-04-19 23:44:58 +0300

    create movie reviews

---
+++ src/components/movie-details/movie-reviews/MovieReviews.jsx
@@ -0,0 +1,52 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const apiKey = "589f3d4f48689702b074a222aea6db87";
+const apiUrl = "https://api.themoviedb.org/3/movie";
+export default function MovieReviews() {
+  const { id: movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/${movieId}/reviews?api_key=${apiKey}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data.results))
+      .catch((error) =>
+        console.error("Error fetching movies from TMDB:", error),
+      );
+  }, [movieId]);
+
+  return (
+    <div className="px-44 pb-10">
+      <h2 className="p-4 text-2xl">
+        <span className="font-bold text-[#266d5d]">|</span> Users reviews
+      </h2>
+
+      {reviews &&
+        reviews.map((review, index) => (
+          <Card className="my-6 flex gap-4 bg-neutral-900 pt-4" key={index}>
+            <CardContent>
+              <div key={index}>
+                <div className="flex">
+                  <img
+                    className="h-32 w-32 rounded-full object-cover p-4"
+                    src={
+                      review.author_details.avatar_path
+                        ? `https://image.tmdb.org/t/p/original/${review.author_details.avatar_path}`
+                        : "https://upload.wikimedia.org/wikipedia/commons/a/af/Default_avatar_profile.jpg"
+                    }
+                    alt={review.author}
+                  />
+                  <h2 className="flex items-center justify-center truncate px-4 text-2xl text-white">
+                    {review.author}
+                  </h2>
+                </div>
+                <p className="px-4 text-lg text-[#9CA4AB]">{review.content}</p>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+    </div>
+  );
+}
