3ad40b219f418b03e808b63ae0119397c273245b
rikimario <mario_martinov@abv.bg>
2024-04-20 22:15:25 +0300

    create search result component

---
+++ src/components/search-result-card/SearchResult.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import Path from "@/paths/paths";
+
+export default function SearchResult({ id, result }) {
+  return (
+    <Link to={`${Path.MovieDetails}/${id}`} className="flex">
+      <div>
+        {result.poster_path ? (
+          <img
+            className="w-32 rounded-lg"
+            key={result.id}
+            src={`https://image.tmdb.org/t/p/w200/${result.poster_path}`}
+            alt=""
+          />
+        ) : (
+          <div>Image not found</div>
+        )}
+      </div>
+
+      <div className="p-4">
+        <div>
+          <h3 className="text-sm font-bold">{result.title}</h3>
+        </div>
+        <p className="text-sm text-[#9CA4AB]">
+          {new Date(result.release_date).getFullYear()}
+        </p>
+      </div>
+    </Link>
+  );
+}
