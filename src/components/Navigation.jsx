41bf984dcf5930e39d99a4d204a5934f086ec21d
rikimario <mario_martinov@abv.bg>
2024-04-21 19:48:31 +0300

    refactor login and register removing modal

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
 import { Input } from "./ui/input";
@@ -7,9 +7,8 @@ import Path from "../paths/paths";
 import SearchResult from "./search-result-card/SearchResult";
 
 const apiKey = "589f3d4f48689702b074a222aea6db87";
-export default function Navigation({ toggleLogin, toggleRegister }) {
+export default function Navigation() {
   const { user, logout } = useContext(AuthContext);
-  const [openNav, setOpenNav] = useState(false);
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
 
@@ -30,16 +29,9 @@ export default function Navigation({ toggleLogin, toggleRegister }) {
       });
   };
 
-  const toggleNav = () => {
-    if (openNav) {
-      setOpenNav(false);
-    } else {
-      setOpenNav(true);
-    }
-  };
-
   const handleClick = () => {
-    setOpenNav(false);
+    setResults([]);
+    setQuery("");
   };
 
   return (
@@ -51,25 +43,20 @@ export default function Navigation({ toggleLogin, toggleRegister }) {
           </Link>
         </div>
         <div className="flex text-[1.3rem]">
-          <ul
-            onClick={handleClick}
-            className={`left-0 right-0 justify-center gap-6 lg:flex lg:gap-14 ${
-              openNav
-                ? "show flex h-screen flex-col items-center text-3xl lg:hidden"
-                : "hidden"
-            }
-            `}
-          >
-            <div className="absolute min-w-[30rem] max-w-[30rem] p-4">
+          <ul className="left-0 right-0 flex justify-center gap-14">
+            <div className="absolute min-w-[30rem] max-w-[30rem]">
               <Input
                 type="text"
                 value={query}
                 onChange={onChange}
                 placeholder="Search"
-                className="hidden text-lg text-black lg:block"
+                className="text-lg text-black"
               />
               {results.length > 0 && (
-                <ul className="scrollbar-thin scrollbar-track-gray-900 scrollbar-thumb-gray-500 max-h-[30rem] overflow-y-auto rounded-md bg-[#0d0c0f] shadow-md">
+                <ul
+                  onClick={handleClick}
+                  className="scrollbar-thin scrollbar-track-gray-900 scrollbar-thumb-gray-500 max-h-[30rem] overflow-y-auto rounded-md bg-[#0d0c0f] shadow-md"
+                >
                   {results.map((result) => (
                     <li className="p-2 hover:bg-[#1b181f]" key={result.id}>
                       <SearchResult
@@ -82,101 +69,45 @@ export default function Navigation({ toggleLogin, toggleRegister }) {
                 </ul>
               )}
             </div>
-
-            <div className="flex w-1/2 flex-col justify-center gap-4 lg:hidden ">
-              {!user && (
-                <>
-                  <button
-                    onClick={toggleRegister}
-                    className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md border px-3 text-sm font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1"
-                  >
-                    Sign Up
-                  </button>
-                  <Link
-                    to={Path.Login}
-                    className="focus-visible:ring-ring text-destructive-foreground hover:bg-destructive/90 inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md bg-[#00925D] px-3 text-sm font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1"
-                  >
-                    Login
-                  </Link>
-                </>
-              )}
-              {user && (
-                <div className="item-center flex flex-col justify-center gap-4">
-                  <Link to={Path.Profile}>
-                    <div className="flex h-9 w-9 items-center justify-center overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
-                      <span className="text-xl font-bold text-gray-600 dark:text-gray-300">
-                        {user.username.charAt(0).toUpperCase()}
-                      </span>
-                    </div>
-                  </Link>
-                  <button
-                    onClick={logout}
-                    className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md border px-3 text-sm font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
-                  >
-                    Sign out
-                  </button>
-                </div>
-              )}
-            </div>
           </ul>
         </div>
 
-        <div className="space-x-4 pr-4">
-          <button
-            onClick={toggleNav}
-            className={`lg:hidden ${openNav ? "hidden" : "show"}`}
-          >
-            <ion-icon size="large" name="menu-outline"></ion-icon>
-          </button>
-
-          <button
-            onClick={toggleNav}
-            className={`lg:hidden ${openNav ? "show" : "hidden"}`}
-          >
-            <ion-icon
-              className="hidden"
-              size="large"
-              name="close-outline"
-            ></ion-icon>
-          </button>
+        <div className="flex space-x-4">
+          {!user && (
+            <>
+              <Link
+                to={Path.Register}
+                className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md border px-3 text-lg font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
+              >
+                Sign Up
+              </Link>
+              <Link
+                to={Path.Login}
+                className="focus-visible:ring-ring text-destructive-foreground hover:bg-destructive/90 inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md bg-[#00925D] px-3 text-lg font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1"
+              >
+                Login
+              </Link>
+            </>
+          )}{" "}
+          {user && (
+            <>
+              <div className="relative inline-flex h-9 w-9 items-center justify-center overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
+                <Link to={Path.Profile}>
+                  <span className="text-xl font-bold text-gray-600 dark:text-gray-300">
+                    {user.username.charAt(0).toUpperCase()}
+                  </span>
+                </Link>
+              </div>
+              <button
+                onClick={logout}
+                className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md border px-3 text-sm font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
+              >
+                Sign out
+              </button>
+            </>
+          )}
         </div>
       </nav>
-
-      <div className="hidden space-x-4 lg:flex">
-        {!user && (
-          <>
-            <Link
-              to={Path.Register}
-              className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md border px-3 text-lg font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
-            >
-              Sign Up
-            </Link>
-            <Link
-              to={Path.Login}
-              className="focus-visible:ring-ring text-destructive-foreground hover:bg-destructive/90 inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md bg-[#00925D] px-3 text-lg font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1"
-            >
-              Login
-            </Link>
-          </>
-        )}{" "}
-        {user && (
-          <>
-            <div className="relative inline-flex h-9 w-9 items-center justify-center overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
-              <Link to={Path.Profile}>
-                <span className="text-xl font-bold text-gray-600 dark:text-gray-300">
-                  {user.username.charAt(0).toUpperCase()}
-                </span>
-              </Link>
-            </div>
-            <button
-              onClick={logout}
-              className="focus-visible:ring-ring border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex h-8 items-center justify-center whitespace-nowrap rounded-md border px-3 text-sm font-medium shadow-sm transition-colors hover:scale-105 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50"
-            >
-              Sign out
-            </button>
-          </>
-        )}
-      </div>
     </div>
   );
 }
