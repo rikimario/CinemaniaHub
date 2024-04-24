c8907642d5c203d26463bb64b59bbfe4a7402057
rikimario <mario_martinov@abv.bg>
2024-04-24 21:48:12 +0300

    add remove func to movie controllers in profile

--- src/components/movie-controlls/MovieControlls.jsx
+++ src/components/movie-controlls/MovieControlls.jsx
@@ -1,81 +1,96 @@
-import { StorageContext } from "@/context/storageContext";
-import { useContext } from "react";
 import { Button } from "../ui/button";
+import { useContext } from "react";
+import { AuthContext } from "@/context/authContext";
+import axios from "axios";
+import toast from "react-hot-toast";
 
-export default function MovieControlls({ movie, type }) {
-  const {
-    addMovieToWatched,
-    addMovieToFavorite,
-    removeFromFavorite,
-    removeFromWatched,
-    removeFromWatchlist,
-  } = useContext(StorageContext);
+export default function MovieControlls({ movie, type, onRemove }) {
+  const { user } = useContext(AuthContext);
+
+  const removeFavorite = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/user/favorite/remove`,
+        {
+          data: { email: user.email, movieId: movie.id },
+        },
+      );
+      onRemove(movie.id);
+      if (response.status.ok) {
+        return response.data;
+      } else {
+        toast.success(response.data.msg);
+        throw new Error(response.data.msg);
+      }
+    } catch (error) {
+      console.log(error);
+      return { msg: "Error removing movie from the liked list" };
+    }
+  };
+
+  const removeWatchlist = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/user/watchlist/remove`,
+        {
+          data: { email: user.email, movieId: movie.id },
+        },
+      );
+      onRemove(movie.id);
+      if (response.status.ok) {
+        return response.data;
+      } else {
+        toast.success(response.data.msg);
+        throw new Error(response.data.msg);
+      }
+    } catch (error) {
+      console.log(error);
+      return { msg: "Error removing movie from the liked list" };
+    }
+  };
+
+  const removeWatched = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/user/watched/remove`,
+        {
+          data: { email: user.email, movieId: movie.id },
+        },
+      );
+      onRemove(movie.id);
+      if (response.status.ok) {
+        return response.data;
+      } else {
+        toast.success(response.data.msg);
+        throw new Error(response.data.msg);
+      }
+    } catch (error) {
+      console.log(error);
+      return { msg: "Error removing movie from the liked list" };
+    }
+  };
 
   return (
     <div className="flex gap-2 pt-3">
       {type === "favorite" && (
-        <>
-          {/* {movie.id && (
-            <Button
-              onClick={() => addMovieToWatched(movie)}
-              variant={"secondary"}
-            >
-              Watched
-            </Button>
-          )} */}
-
-          {movie.id && (
-            <Button
-              variant={"destructive"}
-              onClick={() => removeFromFavorite(movie.id)}
-            >
-              Remove
-            </Button>
-          )}
-        </>
+        <Button onClick={() => removeFavorite()} variant={"destructive"}>
+          Remove
+        </Button>
       )}
 
       {type === "watchlist" && (
         <>
-          {movie.id && (
-            <Button
-              onClick={() => addMovieToWatched(movie)}
-              variant={"secondary"}
-            >
-              Watched
-            </Button>
-          )}
-
-          {movie.id && (
-            <Button
-              variant={"destructive"}
-              onClick={() => removeFromWatchlist(movie.id)}
-            >
-              Remove
-            </Button>
-          )}
+          <Button onClick={() => removeWatchlist()} variant={"destructive"}>
+            Remove
+          </Button>
         </>
       )}
 
       {type === "watched" && (
         <>
-          {movie.id && (
-            <Button
-              onClick={() => addMovieToFavorite(movie)}
-              variant={"secondary"}
-            >
-              Favorite
-            </Button>
-          )}
-
-          {movie.id && (
-            <Button
-              variant={"destructive"}
-              onClick={() => removeFromWatched(movie.id)}
-            >
-              Remove
-            </Button>
-          )}
+          <Button onClick={() => removeWatched()} variant={"destructive"}>
+            Remove
+          </Button>
         </>
       )}
     </div>
