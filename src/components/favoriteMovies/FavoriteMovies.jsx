51f44aaf98f92af100628bccb745f6c96df72252
rikimario <mario_martinov@abv.bg>
2024-04-24 21:45:21 +0300

    add favorite controller

--- src/components/favoriteMovies/FavoriteMovies.jsx
+++ src/components/favoriteMovies/FavoriteMovies.jsx
@@ -1,28 +1,48 @@
-import { useContext } from "react";
-import { StorageContext } from "@/context/storageContext";
+import { AuthContext } from "@/context/authContext";
+import { useContext, useEffect, useState } from "react";
+
 import MovieControlls from "../movie-controlls/MovieControlls";
 
-export default function FavoriteMovies({ movie, type }) {
-  const { favorite } = useContext(StorageContext);
+export default function FavoriteMovies({ type }) {
+  const { user } = useContext(AuthContext);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    try {
+      fetch(`http://localhost:5000/user/favorite/${user.email}`)
+        .then((res) => res.json())
+        .then((data) => {
+          const favoriteMovies = data.movies;
+          setMovies(favoriteMovies);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const removeMovie = (movieId) => {
+    setMovies(movies.filter((m) => m.id !== movieId));
+  };
+
   return (
     <>
       <div className="pl-11 pt-6">
         <h1 className="text-2xl">Favorite movies</h1>
-        <div className="grid grid-cols-5 py-10">
-          {favorite.length === 0 ? (
-            <p className="text-2xl">No favorite movies</p>
-          ) : (
-            favorite.map((movie) => (
-              <div key={movie.id} className="w-56 pb-6">
-                <img
-                  className="h-full w-auto rounded-xl object-fill opacity-50"
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={movie.title}
-                />
-                <MovieControlls type={type} movie={movie} />
-              </div>
-            ))
-          )}
+        <div className="grid grid-cols-5">
+          {movies.map((movie) => (
+            <div key={movie.id} className="w-56 py-12 pb-6">
+              <img
+                className="h-full w-auto rounded-xl object-fill opacity-50"
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <MovieControlls
+                type={type}
+                movie={movie}
+                onRemove={removeMovie}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
