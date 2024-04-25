1c902ae8f8f043e595bc9b07c3721fff3afdcacd
rikimario <mario_martinov@abv.bg>
2024-04-25 22:45:32 +0300

    create not found page

---
+++ src/components/not-found/NotFound.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center text-7xl">
+      <img
+        src="https://webartdevelopers.com/blog/wp-content/uploads/2020/02/404-Error-Lost-And-Alone.gif"
+        alt="Page not found"
+      />
+    </div>
+  );
+}
