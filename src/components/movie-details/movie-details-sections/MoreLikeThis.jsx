2fc081274c2cf461e001483c8639c21dd45b1998
rikimario <mario_martinov@abv.bg>
2024-04-28 17:56:12 +0300

    add emble carousel to more like this

--- src/components/movie-details/movie-details-sections/MoreLikeThis.jsx
+++ src/components/movie-details/movie-details-sections/MoreLikeThis.jsx
@@ -7,10 +7,10 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { AuthContext } from "@/context/authContext";
-import Path from "@/paths/paths";
 import { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
+import Path from "@/paths/paths";
 export default function MoreLikeThis() {
   const { user } = useContext(AuthContext);
   const { id: movieId } = useParams();
@@ -31,36 +31,44 @@ export default function MoreLikeThis() {
       <h2 className="p-4 text-2xl">
         <span className="font-bold text-[#266d5d]">|</span> More Like This
       </h2>
-      <div className="rounded-lg bg-neutral-900">
-        <Carousel>
+      <div className="">
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+            slidesToScroll: 1,
+          }}
+        >
           <CarouselContent className="-ml-1 w-3/5">
             {similars &&
               similars.map((similar, index) => (
                 <CarouselItem
                   key={index}
-                  className="border-x-[1px] border-teal-900 pl-1 md:basis-1/2 lg:basis-1/3"
+                  className="px-6 pl-1 md:basis-1/2 lg:basis-1/3"
                 >
                   <div className="rounded-xl">
                     <div className="h-[24rem] w-full">
                       <img
-                        className="h-full w-auto rounded-xl bg-black object-fill opacity-50"
+                        className="h-full w-auto  rounded-t-2xl bg-black object-fill opacity-50"
                         key={similar.title}
                         src={`https://image.tmdb.org/t/p/original/${similar.poster_path}`}
                         alt={similar.title}
                       />
                     </div>
 
-                    <h2 className="truncate px-4 text-lg">{similar.title}</h2>
-                    <p className="text-md px-4 text-[#9CA4AB]">
-                      {new Date(similar.release_date).getFullYear()}
-                    </p>
+                    <div className="flex flex-col bg-neutral-900">
+                      <h2 className="truncate px-4 text-lg">{similar.title}</h2>
+                      <p className="text-md px-4 text-[#9CA4AB]">
+                        {new Date(similar.release_date).getFullYear()}
+                      </p>
+                    </div>
                   </div>
                   {user && (
-                    <div className="mt-auto flex flex-col justify-end px-2 py-2">
+                    <div className="mt-auto flex flex-col justify-end bg-neutral-900 px-2 py-2">
                       <Button className="bg-[#266d5d]">Watchlist</Button>
                     </div>
                   )}
-                  <div className="flex justify-center px-2 py-2 text-black">
+                  <div className="flex justify-center rounded-b-2xl bg-neutral-900 px-2 py-2 text-black">
                     <Link to={`${Path.MovieDetails}/${similar.id}`}>
                       <Button
                         className="rounded-full"
