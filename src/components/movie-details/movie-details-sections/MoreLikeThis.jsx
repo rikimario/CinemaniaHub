cfcd1a93eb2c9aed1b8c36cbe9872d4b77869d02
rikimario <mario_martinov@abv.bg>
2024-04-30 00:12:23 +0300

    remove add to watchlist btn from more like this

--- src/components/movie-details/movie-details-sections/MoreLikeThis.jsx
+++ src/components/movie-details/movie-details-sections/MoreLikeThis.jsx
@@ -6,26 +6,25 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { AuthContext } from "@/context/authContext";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 import Path from "@/paths/paths";
 export default function MoreLikeThis() {
-  const { user } = useContext(AuthContext);
   const { id: movieId } = useParams();
-  const [similars, setSimilars] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetch(
       `${import.meta.env.VITE_DATABASE_URL}/${movieId}/similar?api_key=${import.meta.env.VITE_API_KEY}`,
     )
       .then((res) => res.json())
-      .then((data) => setSimilars(data.results))
+      .then((data) => setMovies(data.results))
       .catch((error) =>
         console.error("Error fetching movies from TMDB:", error),
       );
   }, [movieId]);
+
   return (
     <div className="px-44 pb-10">
       <h2 className="p-4 text-2xl">
@@ -40,8 +39,8 @@ export default function MoreLikeThis() {
           }}
         >
           <CarouselContent className="-ml-1 w-3/5">
-            {similars &&
-              similars.map((similar, index) => (
+            {movies &&
+              movies.map((similar, index) => (
                 <CarouselItem
                   key={index}
                   className="px-6 pl-1 md:basis-1/2 lg:basis-1/3"
@@ -63,24 +62,14 @@ export default function MoreLikeThis() {
                       </p>
                     </div>
                   </div>
-                  {user && (
-                    <div className="mt-auto flex flex-col justify-end bg-neutral-900 px-2 py-2">
+
+                  <Link to={`${Path.MovieDetails}/${similar.id}`}>
+                    <div className="relative mt-auto flex flex-col justify-end bg-neutral-900 px-2 py-2">
                       <Button className="bg-[#ffc107] text-black hover:text-white">
-                        Watchlist
+                        Read more
                       </Button>
                     </div>
-                  )}
-                  <div className="flex justify-center rounded-b-2xl bg-neutral-900 px-2 py-2 text-black">
-                    <Link to={`${Path.MovieDetails}/${similar.id}`}>
-                      <Button
-                        className="rounded-full"
-                        variant="outline"
-                        size="sm"
-                      >
-                        View more
-                      </Button>
-                    </Link>
-                  </div>
+                  </Link>
                 </CarouselItem>
               ))}
           </CarouselContent>
