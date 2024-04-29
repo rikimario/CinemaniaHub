b44653f548f679ab4acc0e05d950d8c8173e9d48
rikimario <mario_martinov@abv.bg>
2024-04-29 15:28:11 +0300

    add lists btns logic

---
+++ src/services/listServices.js
@@ -0,0 +1,112 @@
+import axios from "axios";
+
+export const fetchWatchlistData = async (userEmail) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/user/watchlist/${userEmail}`,
+    );
+    const { movies } = response.data;
+
+    // Check if movies is an array before using find()
+    if (Array.isArray(movies)) {
+      return movies;
+    } else {
+      console.error(
+        "Watchlist data is not in the expected format:",
+        response.data,
+      );
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const addToWatchlist = async (userEmail, movie) => {
+  try {
+    await axios.post("http://localhost:5000/user/watchlist", {
+      email: userEmail,
+      data: movie,
+    });
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const fetchFavoriteData = async (userEmail) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/user/favorite/${userEmail}`,
+    );
+    const { movies } = response.data;
+
+    // Check if movies is an array before using find()
+    if (Array.isArray(movies)) {
+      return movies;
+    } else {
+      console.error(
+        "Favorite data is not in the expected format:",
+        response.data,
+      );
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const addToFavorite = async (userEmail, movie) => {
+  try {
+    await axios.post("http://localhost:5000/user/favorite", {
+      email: userEmail,
+      data: movie,
+    });
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const fetchWatchedData = async (userEmail) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/user/watched/${userEmail}`,
+    );
+    const { movies } = response.data;
+
+    // Check if movies is an array before using find()
+    if (Array.isArray(movies)) {
+      return movies;
+    } else {
+      console.error(
+        "Watched data is not in the expected format:",
+        response.data,
+      );
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const addToWatched = async (userEmail, movie) => {
+  try {
+    await axios.post("http://localhost:5000/user/watched", {
+      email: userEmail,
+      data: movie,
+    });
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
