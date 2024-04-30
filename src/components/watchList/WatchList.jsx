ea13eee213246f7f9eb266f834c9c55dd65c0c1c
rikimario <mario_martinov@abv.bg>
2024-04-30 22:43:12 +0300

    add a check if the list is empty

--- src/components/watchList/WatchList.jsx
+++ src/components/watchList/WatchList.jsx
@@ -19,7 +19,7 @@ export default function WatchList({ type }) {
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [user.email]);
 
   const removeMovie = (movieId) => {
     setMovies(movies.filter((m) => m.id !== movieId));
@@ -27,25 +27,40 @@ export default function WatchList({ type }) {
   return (
     <>
       <div className="pl-11 pt-6">
-        <h1 className="text-2xl">Watchlist</h1>
-        <div className="grid grid-cols-5">
-          {movies.map((movie) => (
-            <div key={movie.id} className="w-56 py-12 pb-6">
-              <Link to={`${Path.MovieDetails}/${movie.id}`}>
-                <img
-                  className="h-full w-auto rounded-xl object-fill opacity-50 duration-300 hover:scale-105 hover:transform hover:opacity-80"
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={movie.title}
+        <h1 className="text-2xl">Favorite</h1>
+        {movies.length > 0 ? (
+          <div className="grid grid-cols-5">
+            {movies.map((movie) => (
+              <div key={movie.id} className="w-56 py-12 pb-6">
+                {movie.number_of_episodes ? (
+                  <Link to={`${Path.TvDetails}/${movie.id}`}>
+                    <img
+                      className="h-full w-auto rounded-xl object-fill opacity-50 duration-300 hover:scale-105 hover:transform hover:opacity-80"
+                      src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                      alt={movie.name}
+                    />
+                  </Link>
+                ) : (
+                  <Link to={`${Path.MovieDetails}/${movie.id}`}>
+                    <img
+                      className="h-full w-auto rounded-xl object-fill opacity-50 duration-300 hover:scale-105 hover:transform hover:opacity-80"
+                      src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                  </Link>
+                )}
+
+                <MovieControlls
+                  type={type}
+                  movie={movie}
+                  onRemove={removeMovie}
                 />
-              </Link>
-              <MovieControlls
-                type={type}
-                movie={movie}
-                onRemove={removeMovie}
-              />
-            </div>
-          ))}
-        </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-center text-2xl">The list is empty!</p>
+        )}
       </div>
     </>
   );
