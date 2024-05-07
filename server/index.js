ad736f2d81841cf3cae42a875a562ce0ecb56609
rikimario <mario_martinov@abv.bg>
2024-05-07 22:37:01 +0300

    asd

--- server/index.js
+++ server/index.js
@@ -2,29 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://cinema-hub-phi.vercel.app",
-    credentials: true,
-  }),
-);
-
-// app.options("/login", function (req, res) {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://cinema-ki1xsnznb-marios-projects-624972af.vercel.app",
-//   );
-//   res.header("Access-Control-Allow-Methods", "POST");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-
-//   res.sendStatus(200);
-// });
+// app.use(
+//   cors({
+//     origin: "https://cinema-bneralsye-marios-projects-624972af.vercel.app/",
+//   }),
+// );
 
 //* middleware //
 app.use(express.json());
