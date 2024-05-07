68c92dc5202d5bd2cbc77c93a0a4d125ce35b16b
rikimario <mario_martinov@abv.bg>
2024-05-07 18:14:30 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -7,6 +7,18 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://cinema-6hyztsjtu-marios-projects-624972af.vercel.app",
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+
 //* middleware //
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
