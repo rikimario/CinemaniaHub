e4bc5b013c5aea82dc182074979863580527ff8c
rikimario <mario_martinov@abv.bg>
2024-05-07 23:07:59 +0300

    no vercel

--- server/index.js
+++ server/index.js
@@ -2,18 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(
-  cors({
-    origin: "https://cinema-hub-phi.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true,
-  }),
-);
 
 //* middleware //
 app.use(express.json());
