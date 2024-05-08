dd35ca5ec4cf36ae0c84759c9d8095786312af4b
rikimario <mario_martinov@abv.bg>
2024-05-08 17:03:44 +0300

    sss

--- server/index.js
+++ server/index.js
@@ -12,18 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use("/", require("./routes/authRoutes"));
-// CORS middleware
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use("/", require("./routes/authRoutes"));
 
 // * database connection //
 mongoose
