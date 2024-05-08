bdea0852d3c638c4960dec19b1d926f9d5d3ca99
rikimario <mario_martinov@abv.bg>
2024-05-08 14:27:40 +0300

    cors err

--- server/index.js
+++ server/index.js
@@ -13,10 +13,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "cinema-hub-marios-projects-624972af.vercel.app",
-  );
+  const allowedOrigins = [
+    "https://cinema-hub-marios-projects-624972af.vercel.app",
+    "https://cinema-2ot2hrs51-marios-projects-624972af.vercel.app",
+  ];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
