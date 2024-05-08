2bd8e14aa9c6ff6e61e53263625cfe66f75a2b86
rikimario <mario_martinov@abv.bg>
2024-05-08 15:40:43 +0300

    cors to index

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 
@@ -11,23 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "https://cinema-hub-marios-projects-624972af.vercel.app",
-    "https://cinema-2ot2hrs51-marios-projects-624972af.vercel.app",
-  ];
-
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+router.use(cors());
 
 app.use("/", require("./routes/authRoutes"));
 
