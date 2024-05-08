7e01bf3eb9b91dd2d4fdc11e800755ff71790a24
rikimario <mario_martinov@abv.bg>
2024-05-08 22:12:47 +0300

    asd

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const PORT = process.env.PORT;
 
@@ -11,6 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(cors());
 
 app.use("/", require("./routes/authRoutes"));
 
