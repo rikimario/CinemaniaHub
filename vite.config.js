820f66321177b61c80d1a01f6a4fbc247808b798
rikimario <mario_martinov@abv.bg>
2024-05-08 16:22:49 +0300

    qweasd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: { "/server": "localhost:5000" },
+  },
   plugins: [react()],
   resolve: {
     alias: {
