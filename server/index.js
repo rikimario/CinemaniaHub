6429b7f30189115895f7a0f5321b37821f966af6
rikimario <mario_martinov@abv.bg>
2024-05-08 22:26:37 +0300

    aaa

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://cinema-hub-phi.vercel.app", // replace with your domain
+  }),
+);
 
 app.use("/", require("./routes/authRoutes"));
 
