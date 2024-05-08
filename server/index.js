ed0c74e5dd38dbe7652832555e734f278194f42b
rikimario <mario_martinov@abv.bg>
2024-05-08 21:11:20 +0300

    as

--- server/index.js
+++ server/index.js
@@ -6,13 +6,21 @@ const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
 
 //* middleware //
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", require("./routes/authRoutes"));
+// app.use("/", require("./routes/authRoutes"));
 
 // * database connection //
 mongoose
