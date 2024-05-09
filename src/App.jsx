00827c161bd754d981b1dad067971bb3e8b00603
rikimario <mario_martinov@abv.bg>
2024-05-09 12:27:07 +0300

    https://cinema-hub-phi.vercel.app

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ import TvDetails from "./components/TV/tv-details/TvDetails.jsx";
 
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://cinema-hub-git-main-marios-projects-624972af.vercel.app";
+axios.defaults.baseURL = "https://cinema-hub-phi.vercel.app";
 axios.defaults.withCredentials = true;
 
 function App() {
