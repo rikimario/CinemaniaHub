b3da8431de81a7702eb93ac9673326e28d6df309
rikimario <mario_martinov@abv.bg>
2024-05-10 21:48:55 +0300

    consolelog to profile

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -92,6 +92,7 @@ const getProfile = (req, res) => {
   const { token } = req.cookies;
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, {}, (err, user) => {
+      console.log("user", user);
       if (err) throw err;
       res.json(user);
     });
