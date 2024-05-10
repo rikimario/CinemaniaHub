d9bfe7f6b1c45b49d104ba06e9515e7da24419cc
rikimario <mario_martinov@abv.bg>
2024-05-10 22:53:13 +0300

    add token to cookie res! commentout

--- server/index.js
+++ server/index.js
@@ -10,14 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.cookie("token", req.csrfToken(), {
-    sameSite: "None",
-    secure: true,
-    httpOnly: true,
-  });
-  next();
-});
+// app.use((req, res, next) => {
+//   res.cookie("token", req.csrfToken(), {
+//     sameSite: "None",
+//     secure: true,
+//     httpOnly: true,
+//   });
+//   next();
+// });
 
 app.use(
   cors({
