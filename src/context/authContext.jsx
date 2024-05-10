9f229fc750a69c529ca56a009c43130dc77e718b
rikimario <mario_martinov@abv.bg>
2024-05-11 00:01:28 +0300

    uncomment res.cookie

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -12,7 +12,7 @@ export function AuthContextProvider({ children }) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user) {
+    if (user) {
       axios.get("/profile").then(({ data }) => {
         setUser(data);
         localStorage.setItem("user", JSON.stringify(data));
@@ -20,7 +20,7 @@ export function AuthContextProvider({ children }) {
     } else {
       localStorage.removeItem("user");
     }
-  }, [user]);
+  }, []);
 
   const logout = () => {
     axios
