866b9fadafce561f4b7b94238676c921ab099850
rikimario <mario_martinov@abv.bg>
2024-05-11 20:18:27 +0300

    add generated token

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const { generateToken } = require("./helpers/auth");
 
 const app = express();
 
@@ -10,18 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use((req, res, next) => {
-//   res.cookie(
-//     "token",
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hcmlvQGFidi5iZyIsImlkIjoiNjYyOTBlOTRmOTE1ZTk4YzMzODRmNjY2IiwidXNlcm5hbWUiOiJtYXJpb19tYXJ0aW5vdiIsImlhdCI6MTcxNTM3MTUyN30.I9hF7v_BwFE_Ksz6yqL9kscMU4WO1KdGWNUUMGbnR34",
-//     {
-//       sameSite: "None",
-//       secure: true,
-//       httpOnly: true,
-//     },
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.cookie("token", generateToken, {
+    sameSite: "None",
+    secure: true,
+    httpOnly: true,
+  });
+  next();
+});
 
 app.use(
   cors({
