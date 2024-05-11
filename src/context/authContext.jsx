35897a17b981d56cd8dbc455d3357eb57838d868
rikimario <mario_martinov@abv.bg>
2024-05-11 17:32:32 +0300

    return to prev state

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -5,24 +5,18 @@ import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext({});
 
 export function AuthContextProvider({ children }) {
-  // const [user, setUser] = useState(() => {
-  //   const storedUser = localStorage.getItem("user");
-  //   return storedUser ? JSON.parse(storedUser) : null;
-  // });
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    console.log("storedUser", storedUser);
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    } else {
-      axios.get("/profile").then(({ data }) => {
-        setUser(data);
-        localStorage.setItem("user", JSON.stringify(data));
-      });
-    }
+    if (!user) localStorage.removeItem("user");
+    axios.get("/profile").then(({ data }) => {
+      setUser(data);
+      localStorage.setItem("user", JSON.stringify(data));
+    });
   }, []);
 
   const logout = () => {
