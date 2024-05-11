8cf29fe56fa176caacbd66bff7a738861c5ed061
rikimario <mario_martinov@abv.bg>
2024-05-11 23:07:42 +0300

    dist added! update

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "./dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });
 }
 
