b3a027b3aeeaffba7c0939cb05dc41a76f792cfa
rikimario <mario_martinov@abv.bg>
2024-05-23 18:10:28 +0300

    store user in localstorage

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -5,17 +5,20 @@ import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext({});
 
 export function AuthContextProvider({ children }) {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     axios.get("/profile").then(({ data }) => {
-  //       setUser(data);
-  //       localStorage.setItem("user", JSON.stringify(data));
-  //     });
-  //   }
-  // }, [user]);
+  useEffect(() => {
+    if (!user) {
+      axios.get("/profile").then(({ data }) => {
+        setUser(data);
+        localStorage.setItem("user", JSON.stringify(data));
+      });
+    }
+  }, [user]);
 
   const logout = () => {
     axios
