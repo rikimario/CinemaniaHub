1613afa183594bf0ca18ce09b2b1b31c2c528f50
rikimario <mario_martinov@abv.bg>
2024-05-23 17:13:41 +0300

    add http only true to cors

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
+    httpOnly: true,
   }),
 );
 app.use("/", require("./routes/authRoutes"));
