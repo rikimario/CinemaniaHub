6269176e71cc5572fea194f2e18bd6e898a4cde4
rikimario <mario_martinov@abv.bg>
2024-05-29 15:23:51 +0300

    vercel

--- server/index.js
+++ server/index.js
@@ -22,8 +22,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    // origin: "https://cinemania-hub-frontend.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://cinemania-hub-frontend.vercel.app",
+    // origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   }),
